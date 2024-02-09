7fabcb58408eff24a5ec01d43b0eaaff936ee0ae
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-09 19:39:03 +0300

    Initial Commit

---
+++ Shizu/ShizuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShizuApp.swift
+//  Shizu
+//
+//  Created by Ahmet Ozen on 9.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShizuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
