8061f3cfaf6294e8b614fde9567e8f447214e736
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-09 20:45:22 +0300

    working on viewmodel, added function skeletons

--- Shizu/ViewModel/GameViewModel.swift
+++ Shizu/ViewModel/GameViewModel.swift
@@ -13,21 +13,47 @@ import AVFoundation
 class GameViewModel {
     var gameModel: GameModel = .defaultGameModel
     var audioPlayer: AVAudioPlayer?
-    // TODO: Complete Game View Model
+   
+
+    func successSound(){
+        playSound(name: ["awesome","bell","correct","whoop","yes"].randomElement() ?? "whoop")
+    }
     
-    private func playSound(name:String) {
+    func failureSounds(){
+        playSound(name: ["incorrectAnswer","OhNo","weak"].randomElement() ?? "OhNo")
+    }
+    
+    func playNumber(num: Int) {
+        // TODO: complete playNumber
+    }
+    
+    
+    
+    
+    private func playDigit(num: Int) {
+        // TODO: complete playDigit
+    }
+
+    private func digitToName(digit: Int) -> String? {
+        // TODO: complete digitToName
+        return nil
+    }
+    
+    // Play sounds and digits
+    private func playSound(name:String, ext: String = "aiff") {
         guard let soundURL =  Bundle.main.url(forResource: name, withExtension: "aiff") else {
             return
         }
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
             audioPlayer?.prepareToPlay()
-            audioPlayer?.volume = 0.7 // TODO: Update this
+            audioPlayer?.volume = gameModel.volume
             audioPlayer?.play()
         } catch let error  {
             print(error.localizedDescription)
         }
     }
+
     
     
 }
