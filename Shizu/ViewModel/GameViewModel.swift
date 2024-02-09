d41261db263336aeee9c25d15ccc49924fa2da81
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-09 20:35:00 +0300

    created viewModel for game

---
+++ Shizu/ViewModel/GameViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewModel.swift
+//  Shizu
+//
+//  Created by Ahmet Ozen on 9.02.2024.
+//
+
+import Foundation
+import Observation
+import AVFoundation
+
+@Observable
+class GameViewModel {
+    var gameModel: GameModel = .defaultGameModel
+    
+    // TODO: Complete Game View Model
+    
+    
+}
