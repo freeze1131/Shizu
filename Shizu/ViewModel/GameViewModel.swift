64a356db26631c7bf319f9f5d0966ea8a3e09e13
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-09 20:38:45 +0300

    added audioplayer to playsounds, will test

--- Shizu/ViewModel/GameViewModel.swift
+++ Shizu/ViewModel/GameViewModel.swift
@@ -12,8 +12,22 @@ import AVFoundation
 @Observable
 class GameViewModel {
     var gameModel: GameModel = .defaultGameModel
-    
+    var audioPlayer: AVAudioPlayer?
     // TODO: Complete Game View Model
+    
+    private func playSound(name:String) {
+        guard let soundURL =  Bundle.main.url(forResource: name, withExtension: "aiff") else {
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.volume = 0.7 // TODO: Update this
+            audioPlayer?.play()
+        } catch let error  {
+            print(error.localizedDescription)
+        }
+    }
     
     
 }
