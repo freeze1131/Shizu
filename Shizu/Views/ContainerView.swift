befc40ddeef7fb72d8621fa649605331db477bef
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-10 13:32:41 +0300

    working on splashscreen

---
+++ Shizu/Views/ContainerView.swift
@@ -0,0 +1,23 @@
+//
+//  ContainerView.swift
+//  Shizu
+//
+//  Created by Ahmet Ozen on 10.02.2024.
+//
+
+import SwiftUI
+
+struct ContainerView: View {
+    @State private var splashScreenIsPresented:Bool = true
+    var body: some View {
+        if splashScreenIsPresented {
+            SplashScreenView(isPresented: $splashScreenIsPresented)
+        } else {
+            MainView()
+        }
+    }
+}
+
+#Preview {
+    ContainerView()
+}
